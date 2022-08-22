8b7f916b3e1af4e6c51711c23f51529b347f6a92
Zheyu Zhang <jimzhang826@sjtu.edu.cn>
2022-08-22 20:56:47 +0800

    merge

--- GPU_Platform/platform_milestone_1.0/merged/length_test.cu
+++ GPU_Platform/platform_milestone_1.0/merged/length_test.cu
@@ -22,13 +22,12 @@ ARGS:
 
 int taskType;
 float taskLen;
-int cpuUnitTask;
-__device__ int gpuUnitTask;
+int unitTask;
 __device__ float deviceData[2048];
 
 void cpuTaskFunc(float cpuTaskLen) {
   float c = 0;
-  long i = cpuTaskLen * cpuUnitTask;
+  long i = cpuTaskLen * unitTask;
   for (long j = 0; j < i; ++j) {
     c += 98765.4321 / 654.321;
     c -= 98765.4321 / 654.321;
@@ -37,7 +36,7 @@ void cpuTaskFunc(float cpuTaskLen) {
   }
 }
 
-__global__ void gpuTaskFunc(float gpuTaskLen) {
+__global__ void gpuTaskFunc(float gpuTaskLen, int gpuUnitTask) {
   long i = threadIdx.x + blockIdx.x * blockDim.x;
   long j = gpuTaskLen * gpuUnitTask;
   float a = 9876.54321, b = 543.21;
@@ -50,9 +49,9 @@ __global__ void gpuTaskFunc(float gpuTaskLen) {
 int main(int argc, char **argv) {
   taskType = atoi(argv[1]);
   taskLen = atof(argv[2]);
+  unitTask = atoi(argv[3]);
 
   if (taskType == 0) {
-    cpuUnitTask = atoi(argv[3]);
     while (1) {
       struct timeval startTime;
       struct timeval endTime;
@@ -64,12 +63,10 @@ int main(int argc, char **argv) {
       printf("%ld\n", duration);
     }
   } else {
-    int tmp = atoi(argv[3]);
-    cudaDebugCall(cudaMemcpyToSymbol(gpuUnitTask, &tmp, sizeof(int)));
-    gpuTaskFunc<<<2, 1024, 0, 0>>>(250); // gpu warm up
+    gpuTaskFunc<<<2, 1024, 0, 0>>>(250, unitTask); // gpu warm up
     cudaDebugCall(cudaDeviceSynchronize());
     while (1) {
-      gpuTaskFunc<<<2, 1024, 0, 0>>>(taskLen);
+      gpuTaskFunc<<<2, 1024, 0, 0>>>(taskLen, unitTask);
       cudaDebugCall(cudaDeviceSynchronize());
     }
   }
