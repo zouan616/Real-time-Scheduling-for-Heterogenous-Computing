dd484459ab3dffcea1bcddc061c928d8f16e04e0
Xu-8482 <85929237+Xu-8482@users.noreply.github.com>
2022-11-18 12:34:43 +0800

    Update driver.sh

--- GPU_Platform/platform_milestone_1.0/merged/driver.sh
+++ GPU_Platform/platform_milestone_1.0/merged/driver.sh
@@ -5,7 +5,7 @@ function daemon() {
   size1=$(stat trace | grep Size)
   size2=null
   while true; do
-    sleep 300
+    sleep 3000
     size2=$(stat trace | grep Size)
     if [ "$size1" == "$size2" ]; then
       pkill -f ./scheduling_experiment
@@ -21,16 +21,16 @@ function do_test() {
   u_end=$3
   scale=$4
   level=$5
-  for total_util in $(seq "$u_start" 10 "$u_end"); do
+  for total_util in $(seq "$u_start" 20 "$u_end"); do
     echo total_util "$total_util" >>trace
     success_count=0
-    for iteration in {1..100}; do
+    for iteration in {1..50}; do
       echo iteration "$iteration" >>trace
       ./data_generator "$data_type" "$total_util" "$scale" "$level"
       # cat pthreadData.dat >>pthreadData.db
       # sed -n "$iteration"p pthreadData.db >pthreadData.dat
-      for prio in {0..119}; do
-        echo "$prio" >>trace
+      for prio in {0..1}; do
+        # echo "$prio" >>trace
         if ./scheduling_experiment "$prio"; then
           ((success_count += 1))
           echo success >>trace
@@ -43,7 +43,7 @@ function do_test() {
 }
 
 echo >trace
-/usr/local/cuda-11.4/bin/nvcc data_generator.cu -o data_generator
-/usr/local/cuda-11.4/bin/nvcc scheduling_experiment.cu -o scheduling_experiment
-daemon &
-do_test 1 180 180 0 0
+/usr/local/cuda-10.2/bin/nvcc data_generator.cu -o data_generator
+/usr/local/cuda-10.2/bin/nvcc scheduling_experiment.cu -o scheduling_experiment
+# daemon &
+do_test 2 200 440 2 0
